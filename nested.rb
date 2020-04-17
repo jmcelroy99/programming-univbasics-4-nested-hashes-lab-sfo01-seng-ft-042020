7bad6fa2ccc2d476745deea96a5df8792019fd6d
energetic-commit-5967 <jmcelroy@plexustech.net>
2020-04-17 20:41:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-    rich = programmer_hash[:dennis_ritchie][:known_for]
+    rich = programmer_hash[:dennis_ritchie][:language]
     rich
 
 end
